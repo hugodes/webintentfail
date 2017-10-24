85b9aef616808cf21b13642ef6458c03bff1312b
Hugo Des <hugo.deslongchamps@gmail.com>
2017-10-24 17:29:14 +0200

    Update to better use getIntent()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,17 +25,29 @@ export class MyApp {
 
 
       if(platform.is('android')){
-        let intent = this.webIntent.getIntent();
+        let intent = this.webIntent.getIntent().then((value) => {
+          console.log('intent1 OK');
+          console.log(value);
+        }, (reason) => {
+          console.log('intent1 KO');
+          console.log(reason);
+        });
         let intent2 = this.webIntent.onIntent();
-        console.log(intent);
+        console.log('intent2');
         console.log(intent2);
         let resume = platform.resume;
         resume.subscribe((event) => {
           platform.ready()
             .then(() => {
-              let intent = this.webIntent.getIntent();
+              let intent = this.webIntent.getIntent().then((value) => {
+                console.log('intent1 OK resume');
+                console.log(value);
+              }, (reason) => {
+                console.log('intent1 KO resume');
+                console.log(reason);
+              });
               let intent2 = this.webIntent.onIntent();
-              console.log(intent);
+              console.log('intent2');
               console.log(intent2);
             });
         });
